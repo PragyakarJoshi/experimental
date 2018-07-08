39cd8686e396b4b690c536854f9d15ad4bd4f766
Pragyakar Joshi <pragyakar75@gmail.com>
2018-07-08 20:07:03 +0545

    UI update

--- text-visibility/app.js
+++ text-visibility/app.js
@@ -1,17 +1,17 @@
 
 window.onload = function() {
-    document.getElementById("box").style.backgroundColor = generateColor();
+    document.getElementById("box").style.backgroundColor = makeColor();
 };
 
-function generateColor(){
+function makeColor(){
     var r = Math.floor(Math.random() * 255) + 1;
     var g = Math.floor(Math.random() * 255) + 1;
     var b = Math.floor(Math.random() * 255) + 1;
     var color = 'rgb('+ r + ',' + g +','+ b +')';
+    document.getElementById("col-red").innerHTML = r;
+    document.getElementById("col-green").innerHTML = g;
+    document.getElementById("col-blue").innerHTML = b;
     console.log(color);
     return color;
 }
-
-
-
 
