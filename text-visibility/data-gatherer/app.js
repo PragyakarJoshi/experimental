72788bd341170b7f3f9a424aba343dfbdf6b8e4e
Pragyakar Joshi <pragyakar75@gmail.com>
2018-07-10 17:01:16 +0545

    structuring

--- text-visibility/data-gatherer/app.js
+++ text-visibility/data-gatherer/app.js
@@ -1,56 +1,55 @@
-let r, g, b, color, database;
-  
-  // Initialize Firebase
-  var config = {
+window.onload = function () {
+    makeColor();
+};
+
+let r, g, b, color, fbdb;
+
+var config = {
     apiKey: "AIzaSyCa0TlNwRg1yYij8jMVwAKt-UfsH9AC7Ms",
     authDomain: "text-visibility.firebaseapp.com",
     databaseURL: "https://text-visibility.firebaseio.com",
     projectId: "text-visibility",
     storageBucket: "",
     messagingSenderId: "459974708907"
-  };
-  firebase.initializeApp(config);
-  database = firebase.database();
-
-
-window.onload = function() {
-    document.getElementById("box").style.backgroundColor = makeColor();
 };
+firebase.initializeApp(config);
+fbdb = firebase.database();
 
-function makeColor(){
+function makeColor() {
     r = Math.floor(Math.random() * 255) + 1;
     g = Math.floor(Math.random() * 255) + 1;
     b = Math.floor(Math.random() * 255) + 1;
-    color = 'rgb('+ r + ',' + g +','+ b +')';
+    color = 'rgb(' + r + ',' + g + ',' + b + ')';
     document.getElementById("col-red").innerHTML = r;
     document.getElementById("col-green").innerHTML = g;
     document.getElementById("col-blue").innerHTML = b;
-    console.log(color);
+    document.getElementById("box").style.backgroundColor = color;
     return color;
 }
 
 
 function saveData(obj) {
     textValue = obj.value;
-    let colorDatabase = database.ref('colors');
+    let colorDb = fbdb.ref('colors');
 
     var data = {
-        r: r, 
-        b: b, 
+        r: r,
+        b: b,
         g: g,
-        label : textValue
+        label: textValue
     }
-    console.log(data);
 
-    let color = colorDatabase.push(data, finished);
+    let color = colorDb.push(data, check);
     console.log("Firebase granted key: " + color.key);
+    console.log(data);
 }
 
-function finished(err) {
-    if (err) {
-        console.log("error");
-        console.log(err);
+function check(error) {
+    if (error) {
+        console.log("Failed to Save data.");
+        console.log(error);
     } else {
-        console.log("success");
+        console.log("Sucessfully saved data to DB");
+        makeColor();
     }
 }
